15c833078280f9e2766ae53b1d8b1ca7c2e7b884
BarryHoll <bchollgithub@gmail.com>
2015-05-29 16:36:38 +0100

    Adding MovieFileFormatPlayer (for real this time...)

---
+++ AdapterPattern/src/com/bchollywood/MovieFileFormatPlayer.java
@@ -0,0 +1,9 @@
+package com.bchollywood;
+
+public interface MovieFileFormatPlayer {
+	
+	public void playAvi(String fileName);
+	public void playWmv(String fileName);
+	public void playMp4(String fileName);
+
+}
