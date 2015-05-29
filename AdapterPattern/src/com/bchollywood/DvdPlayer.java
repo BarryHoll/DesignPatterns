d3308ccfe677a8af754bd2dc1091187715f65835
BarryHoll <bchollgithub@gmail.com>
2015-05-29 17:08:43 +0100

    Adding DvdPlayer which implements MovieDiscPlayer but uses MovieAdapter.

---
+++ AdapterPattern/src/com/bchollywood/DvdPlayer.java
@@ -0,0 +1,23 @@
+package com.bchollywood;
+
+public class DvdPlayer implements MovieDiscPlayer {
+	
+	MovieAdapter movieAdapter;
+
+	@Override
+	public void play(String movieType, String fileName) {
+
+		if (movieType.equalsIgnoreCase("DVD")) {					// The DVD Player, out of the box,
+			System.out.println("Playing DVD. Title: " + fileName);  //   can play DVDs.
+		} else if (movieType.equalsIgnoreCase("avi") 
+				|| movieType.equalsIgnoreCase("wmv") 				// To play movie file formats,
+				|| movieType.equalsIgnoreCase("mp4")) {     		//   movieAdapter is used.
+			movieAdapter = new MovieAdapter(movieType);
+			movieAdapter.play(movieType, fileName);
+		} else {
+			System.out.println("Error! Movie format " + movieType   // And if it's not on the list
+					+ " can not be played.");						// it can't come in.
+		}
+
+	}
+}
